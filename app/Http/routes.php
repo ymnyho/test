edb95bfe7d1b1c3e5aff221543c4fe39035d162b
ymnyho <ymnyho@gmail.com>
2016-01-23 18:16:14 +0800

    v3 view初始

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,17 +14,30 @@
 //throw new \Exception('Tracy works!');
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
-Route::get('dashboard',function(){
-	return 'dashboard';
+Route::get('hello',function(){
+	return view('home.hello');
 });
 
 
+/*
+page10:
+Route::get('hello',function(){
+	return view('home.hello');
+});
 
-/*v2,ok*/
+
+Route::get('hello/{name?}',['as'=>"hello_name",function($name = 'everybody'){
+	return "hello hong,".$name;
+}]);
+
+
+
+v2,ok
 Route::group(['prefix' => 'admin'],function(){
+	
 	Route::get('account',function(){
 		return 'admin account';
 	});
@@ -34,29 +47,29 @@ Route::group(['prefix' => 'admin'],function(){
 	});
 });
 
-/*v1,ok
+v1,ok
 Route::get('admin/account',function(){
 	return 'admin account';
 });
 
 Route::get('admin/dashboard',function(){
 	return 'admin dashboard';
-});*/
+});
 
-/*Route::get('hello/{name?}',['as'=>"hello_name",function($name = 'everybody'){
-	return "hello hong,".$name;
-}]);
+
 
 
 Route::get('hello_1/{id?}',function($id = '123'){
 	return "id:".$id;
 })
-->where('id','[0-9]+');*/
+->where('id','[0-9]+');
 
 
-/*Route::get('hello/{id?}',function($id = '123'){
+Route::get('hello/{id?}',function($id = '123'){
 	return "id:".$id;
-});*/
+});
+*/
+
 
 /*
 |--------------------------------------------------------------------------
---
+++ resources/views/home/war/hello.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>hello home title</title>
+    </head>
+    <body>
+        <h1>war hello home</h1>
+    </body>
+</html>
